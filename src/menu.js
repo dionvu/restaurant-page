2c0666c7905bd49a4d080c8dd98760bf3e218d46
Dion Vu <dionvu11@gmail.com>
2024-02-12 18:44:20 -0800

    Added temporary placeholders for food items.

--- src/menu.js
+++ src/menu.js
@@ -1,11 +1,51 @@
+import './style/menu.css'
+import burger from './assets/burger.jpg'
+
+class Item {
+  constructor(name, price, image) {
+    this.name = name;
+    this.price = price;
+    this.image = image;
+  };
+}
+
+let menu = [
+  new Item('Burger', 100, ""),
+  new Item('Hot Dog', 100, ""),
+  new Item('Burger', 100, ""),
+  new Item('Hot Dog', 100, ""),
+  new Item('Burger', 100, ""),
+  new Item('Hot Dog', 100, ""),
+  new Item('Burger', 100, ""),
+  new Item('Hot Dog', 100, ""),
+  new Item('Hot Dog', 100, "")
+];
+
 function createMenu() {
   const content = document.getElementById('content');
   content.innerHTML = '';
 
-  const header = document.createElement('h1');
-  header.textContent = 'MENU';
-  document.body.appendChild(header);
-  content.appendChild(header);
+  for (let i = 0; i < 9; i++) {
+    const item = document.createElement('div');
+    item.classList.add('menu-item');
+
+    const name = document.createElement('h1');
+    name.classList.add('menu-item-name');
+    name.textContent = menu[i].name;
+
+    const image = document.createElement('img');
+    image.classList.add('menu-item-image');
+    image.src = burger;
+
+    const price = document.createElement('h1');
+    price.classList.add('menu-item-price');
+    price.textContent = '$' + menu[i].price;
+
+    content.appendChild(item);
+    item.appendChild(name);
+    item.appendChild(image);
+    item.appendChild(price);
+  }
 }
 
 export default createMenu;
