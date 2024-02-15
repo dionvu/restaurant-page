44b6b026dbc96c8271a996e7c972dd4367b636f6
Dion Vu <dionvu11@gmail.com>
2024-02-14 21:22:45 -0800

    Created a header with image, and order online text.

--- src/home.js
+++ src/home.js
@@ -1,18 +1,50 @@
 import './style/nav.css'
 import './style/home.css'
+import headerImg from '../assets/home_header.jpg'
 
-function createHome() {
+function home() {
   const container = document.getElementById('container');
   container.innerHTML = '';
 
   const content = document.createElement('div');
   content.setAttribute('id', 'content');
 
-  const header = document.createElement('h1');
-  header.textContent = 'HOME';
-  content.appendChild(header);
+  header(content);
 
   container.appendChild(content);
 }
 
-export default createHome;
+function header(content) {
+  const header = document.createElement('div');
+  const headerImage = document.createElement('img');
+
+  headerImage.src = headerImg;
+  headerImage.id = 'header-image';
+
+  header.id = 'header';
+
+  header.appendChild(headerImage);
+  content.appendChild(header);
+
+  headerOverlay(header);
+}
+
+function headerOverlay(header) {
+  const headerOverlay = document.createElement('div');
+  headerOverlay.id = 'header-overlay';
+
+  const headerBig = document.createElement('h1');
+  headerBig.id = 'header-big';
+
+  const topText = document.createElement('h2');
+  topText.id = 'header-top';
+
+  headerBig.textContent = 'ORDER ONLINE';
+  topText.textContent = 'Delicious food all the time';
+
+  header.appendChild(headerOverlay);
+  headerOverlay.appendChild(headerBig);
+  headerOverlay.appendChild(topText);
+}
+
+export default home;
