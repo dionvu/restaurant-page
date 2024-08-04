92e0e233b462724f287336a642baf38bbbf99993
Dion Vu <dionvu11@gmail.com>
2024-08-04 11:40:38 -0700

    Rando changes

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import './style/nav.css'
-import home from './home.js'
-import menu from './menu.js'
-import contact from './contact.js'
+import "./style/nav.css";
+import home from "./home.js";
+import menu from "./menu.js";
+import contact from "./contact.js";
 
-const homeButton = document.getElementById('home-button');
-const menuButton = document.getElementById('menu-button');
-const contactButton = document.getElementById('contact-button');
+const homeButton = document.getElementById("home-button");
+const menuButton = document.getElementById("menu-button");
+const contactButton = document.getElementById("contact-button");
 
-homeButton.addEventListener('click', home);
-menuButton.addEventListener('click', menu);
-contactButton.addEventListener('click', contact);
+homeButton.addEventListener("click", home);
+menuButton.addEventListener("click", menu);
+contactButton.addEventListener("click", contact);
 
 home();
